5ab7db9a9334229b6cde379b2f90438b2bcf1c05
GopalKrishna2609 <91457773+GopalKrishna2609@users.noreply.github.com>
2022-06-02 16:23:33 +0530

    Add files via upload
    Added Unit testing project

---
+++ ICalculatorLib.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorLib
+{
+
+public class ICalculatorLib
+    {
+       public double Add(double a, double b)
+        {
+            return a + b;
+        }
+        public double Sub(double a, double b)
+        {
+            return a - b;
+        }
+        public double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+        public double Divide(double a, double b)
+        {
+            return a / b; 
+        }
+
+    }
+}
